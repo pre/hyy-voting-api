171692dd1a744a440318df59391d0bd77bb7bc23
Petrus Repo <petrus.repo@iki.fi>
2016-08-10 11:43:33 +0300

    doc (Haka) Document HY attr format for student number

--- app/models/haka/person.rb
+++ app/models/haka/person.rb
@@ -19,7 +19,15 @@ module Haka
     end
 
     # Parse actual student id from University of Helsinki's format:
-    #   urn:schac:personalUniqueCode:fi:yliopisto.fi:x873421 => 873421
+    #   urn:mace:terena.org:schac:personalUniqueCode:int:studentID:helsinki.fi:165934
+    #
+    # See also:
+    #   http://www.helsinki.fi/atk/luvat/ldap/doc/index.html#henkiloluokat_schacPersonalUniqueCode
+    #   https://confluence.csc.fi/display/HAKA/funetEduPersonSchema2dot2#funetEduPersonSchema2dot2-schacPersonalUniqueCode
+    #
+    # Returns the number value of the last URN attribute:
+    #   raw "urn:schac:personalUniqueCode:fi:yliopisto.fi:x873421"
+    #   will return: "873421"
     def parse_student_number(raw)
       raw.split(":").last.delete('^0-9')
     end
