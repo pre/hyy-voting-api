33cd2495122d78917ab0fdb0072d759b2c1ad433
Petrus Repo <petrus.repo@iki.fi>
2016-08-26 11:38:12 +0300

    chore (Rollbar) Update configuration

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,11 +2,10 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV.fetch 'ROLLBAR_ACCESS_TOKEN'
-
-  # Here we'll disable in 'test':
-  if Rails.env.test? || Rails.env.development?
+  if Rails.env.test? or Rails.env.development?
     config.enabled = false
+  else
+    config.access_token = ENV.fetch 'ROLLBAR_ACCESS_TOKEN'
   end
 
   # By default, Rollbar will try to call the `current_user` controller method
@@ -47,4 +46,11 @@ Rollbar.configure do |config|
   # config.use_sidekiq
   # You can supply custom Sidekiq options:
   # config.use_sidekiq 'queue' => 'default'
+
+  # If you run your staging application instance in production environment then
+  # you'll want to override the environment reported by `Rails.env` with an
+  # environment variable like this: `ROLLBAR_ENV=staging`. This is a recommended
+  # setup for Heroku. See:
+  # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
+  config.environment = ENV['ROLLBAR_ENV'] || Rails.env
 end
