6ba6ebd4252eaac52d92ae2bd06f05be3b1d70cd
Petrus Repo <petrus.repo@iki.fi>
2016-09-02 12:19:54 +0300

    feat (RuntimeConfig) Voting closes daily at given hour

--- app/models/runtime_config.rb
+++ app/models/runtime_config.rb
@@ -2,6 +2,7 @@
 # For static configuration, see `config/initializers/000_config.rb`
 class RuntimeConfig
 
+  # User can sign in to check his eligibility in Halloped elections.
   def self.eligibility_signin_active?
     now = Time.now
 
@@ -9,20 +10,42 @@ class RuntimeConfig
       now <= Vaalit::Config::ELIGIBILITY_SIGNIN_ENDS_AT
   end
 
+  # User can sign in to vote.
   def self.vote_signin_active?
     now = Time.now
 
-    Vaalit::Config::VOTE_SIGNIN_STARTS_AT <= now &&
-      now <= Vaalit::Config::VOTE_SIGNIN_ENDS_AT
+    voting_day?(now) && voting_time?(now)
   end
 
+  # User can submit a vote.
+  #
   # Allow a grace period to submit votes.
   # Only for users who have signed in before the sign in ended.
   def self.voting_active?
     now = Time.now
 
-    Vaalit::Config::VOTE_SIGNIN_STARTS_AT <= now &&
-      Time.now <= Vaalit::Config::VOTE_SIGNIN_ENDS_AT + Vaalit::Config::VOTING_GRACE_PERIOD_MINUTES
+    signin_has_started?(now) &&
+      now <= voting_ends_at? &&
+      voting_time_with_grace_period?(now)
   end
 
+  private_class_method def self.signin_has_started?(now)
+    Vaalit::Config::VOTE_SIGNIN_STARTS_AT <= now
+  end
+
+  private_class_method def self.voting_day?(now)
+    signin_has_started?(now) && now <= Vaalit::Config::VOTE_SIGNIN_ENDS_AT
+  end
+
+  private_class_method def self.voting_time?(now)
+    now <= Time.parse(Vaalit::Config::VOTE_SIGNIN_DAILY_CLOSING_TIME)
+  end
+
+  private_class_method def self.voting_time_with_grace_period?(now)
+    now <= Time.parse(Vaalit::Config::VOTE_SIGNIN_DAILY_CLOSING_TIME) + Vaalit::Config::VOTING_GRACE_PERIOD_MINUTES
+  end
+
+  private_class_method def self.voting_ends_at?
+    Vaalit::Config::VOTE_SIGNIN_ENDS_AT + Vaalit::Config::VOTING_GRACE_PERIOD_MINUTES
+  end
 end
---
+++ spec/models/runtime_config_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe RuntimeConfig, type: :model do
+
+  context "voting has not started yet" do
+    before do
+      stub_const("Vaalit::Config::VOTE_SIGNIN_STARTS_AT", 1.hour.from_now)
+      stub_const("Vaalit::Config::VOTE_SIGNIN_ENDS_AT", 2.hours.from_now)
+    end
+
+    it "vote signin is not active" do
+      expect(RuntimeConfig.vote_signin_active?).to be false
+    end
+
+    it "voting is not active" do
+      expect(RuntimeConfig.voting_active?).to be false
+    end
+  end
+
+  context "voting day is over but continues the next day" do
+    before do
+      stub_const("Vaalit::Config::VOTE_SIGNIN_STARTS_AT", 3.days.ago)
+      stub_const("Vaalit::Config::VOTE_SIGNIN_ENDS_AT", 1.day.from_now)
+      stub_const("Vaalit::Config::Vaalit::Config::VOTING_GRACE_PERIOD_MINUTES", 5.minutes)
+    end
+
+    context "grace period is active" do
+      before do
+        # Use Timecop gem if test starts to fail for running too long.
+        stub_const("Vaalit::Config::VOTE_SIGNIN_DAILY_CLOSING_TIME",
+                   Time.now.strftime('%H:%M'))
+      end
+
+      it "vote signin is not active" do
+        expect(RuntimeConfig.vote_signin_active?).to be false
+      end
+
+      it "voting is active" do
+        expect(RuntimeConfig.voting_active?).to be true
+      end
+    end
+
+    context "grace period is over" do
+      before do
+        stub_const("Vaalit::Config::VOTE_SIGNIN_DAILY_CLOSING_TIME",
+                   301.seconds.ago.strftime('%H:%M'))
+      end
+
+      it "vote signin is not active" do
+        expect(RuntimeConfig.vote_signin_active?).to be false
+      end
+
+      it "voting is active" do
+        expect(RuntimeConfig.voting_active?).to be false
+      end
+    end
+
+  end
+
+  context "voting has ended" do
+    before do
+      stub_const("Vaalit::Config::VOTE_SIGNIN_STARTS_AT", 3.days.ago)
+      stub_const("Vaalit::Config::Vaalit::Config::VOTING_GRACE_PERIOD_MINUTES", 5.minutes)
+    end
+
+    context "grace period is active" do
+      before do
+        stub_const("Vaalit::Config::VOTE_SIGNIN_ENDS_AT", 299.seconds.ago)
+      end
+
+      it "vote signin is not active" do
+        expect(RuntimeConfig.vote_signin_active?).to be false
+      end
+
+      it "voting is active" do
+        expect(RuntimeConfig.voting_active?).to be true
+      end
+    end
+
+    context "grace period is over" do
+      before do
+        stub_const("Vaalit::Config::VOTE_SIGNIN_ENDS_AT", 300.seconds.ago)
+      end
+
+      it "vote signin is not active" do
+        expect(RuntimeConfig.vote_signin_active?).to be false
+      end
+
+      it "voting is not active" do
+        expect(RuntimeConfig.voting_active?).to be false
+      end
+    end
+  end
+end
