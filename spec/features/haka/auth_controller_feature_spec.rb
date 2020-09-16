b2a0e7eefa129e86a23f24777b55e2b3c6378dee
Petrus Repo <petrus.repo@iki.fi>
2020-09-16 10:38:00 +0300

    Disable Selenium tests as Chromedriver is too much of a hassle

--- spec/features/haka/auth_controller_feature_spec.rb
+++ spec/features/haka/auth_controller_feature_spec.rb
@@ -1,33 +1,36 @@
 require 'rails_helper'
 
-module Haka
-  describe "AuthController", :type => :feature do
-    context "sign in process" do
-      before :each do
-        haka_user_student_number = 8734
-        election = FactoryGirl.create :election, :edari_election
-        @voter = FactoryGirl.create :voter, :with_voting_right,
-                                    election: election,
-                                    student_number: haka_user_student_number
+## Selenium test is disabled as Chromedriver is too much of a hassle to keep working
+## between elections every 2 year.
 
-        allow(RuntimeConfig).to receive(:vote_signin_active?).and_return true
-      end
+# module Haka
+#   describe "AuthController", :type => :feature do
+#     context "sign in process" do
+#       before :each do
+#         haka_user_student_number = 8734
+#         election = FactoryGirl.create :election, :edari_election
+#         @voter = FactoryGirl.create :voter, :with_voting_right,
+#                                     election: election,
+#                                     student_number: haka_user_student_number
 
-      it "signs teppo testaaja in" do
-        visit '/haka/auth/new'
+#         allow(RuntimeConfig).to receive(:vote_signin_active?).and_return true
+#       end
 
-        within("form") do
-          fill_in 'Username', :with => "teppo"
-          fill_in 'Password', :with => "testaaja"
-        end
+#       it "signs teppo testaaja in" do
+#         visit '/haka/auth/new'
 
-        click_button 'Login'
+#         within("form") do
+#           fill_in 'Username', :with => "teppo"
+#           fill_in 'Password', :with => "testaaja"
+#         end
 
-        #TODO: Conditionally click Accept when Haka asks a confirmation
-        # click_button 'Accept'
+#         click_button 'Login'
 
-        expect(page).to have_content 'Vaalin nimi'
-      end
-    end
-  end
-end
+#         #TODO: Conditionally click Accept when Haka asks a confirmation
+#         # click_button 'Accept'
+
+#         expect(page).to have_content 'Vaalin nimi'
+#       end
+#     end
+#   end
+# end
