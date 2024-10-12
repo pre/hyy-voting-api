72bb1c219db27c5736adc346219a51b4f0dea971
Petrus Repo <petrus.repo@iki.fi>
2024-10-12 13:47:56 +0300

    Remove old framework defaults

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,12 +1,2 @@
-# Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-# Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = true
-
-# Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
-
-# Configure SSL options to enable HSTS with subdomains. Previous versions had false.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
-
 # For Rails 7.2
 Rails.application.config.active_record.commit_transaction_on_non_local_return = true
