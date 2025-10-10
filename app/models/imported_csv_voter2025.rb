aeac6276d0b9d57cc513a13d07c6ae61124845c7
Petrus Repo <petrus.repo@iki.fi>
2025-10-10 09:48:17 +0300

    Voter import 2025

--- app/models/imported_csv_voter2025.rb
+++ app/models/imported_csv_voter2025.rb
@@ -2,29 +2,27 @@ class ImportedCsvVoter2025 < ImportedVoter
 
   # Assumes there's no header
   # 2025
-  # student_number;ssn;lastname;firstnames;email;faculty;start_year
-  #   0              1     2         3       4       5     6
+  # opiskelijanro;henkilotunnus;sukunimi;etunimet;email;tdk;opintojen_aloitusvuosi;opintojen_aloituslukukausi
+  #   0              1            2         3       4    5     6                       7
+  #
+  # Example row:
+  # 012345677;010101A1111;Lastname;First Name Multiple;firstname.lastname@helsinki.fi;FTDK;2025;S
   def convert(data)
     @student_number = data[0]
     @ssn = data[1]
     @name = "#{data[2]}, #{data[3]}"
     @email = data[4]
     @faculty_abbreviation = parse_abbr(data[5])
-    @start_year = parse_year_from_date(data[6])
+    @start_year = data[6]
+  rescue StandardError => e
+    puts "Failed with #{data.inspect}"
+    raise e
   end
 
   def parse_extent_of_studies(str)
     return nil if str.blank?
 
     str.to_i
-  end
-
-  # date: "1.8.2020"
-  # return: "2020"
-  def parse_year_from_date(date_str)
-    return nil if date_str.blank?
-
-    Time.parse(date_str).year
   end
 
   # Data might contain multiple faculties, pick the first one
